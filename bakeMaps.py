ec2a49c6f8a612a7e1bfba8ae7b29d37c5c1d9cf
TheFamousRat <minone.roy@free.fr>
2020-04-16 16:18:53 +0200

    Added options in exportAll.py, normal maps correctly set in bakeMaps.py

--- bakeMaps.py
+++ bakeMaps.py
@@ -3,18 +3,18 @@ import os
 
 ###### PARAMETERS ######
 
-outputFolder = 'meshes'#Relative path from .blend file
+outputFolder = 'textures'#Relative path from .blend file
 marginParam = 8.0
 defaultBakedImageDim = 512
 meshesDim = {
-"Scarf" : 512,
+"Scarf" : 128,
 "Eyes" : 128,
 "Nose" : 256,
 "Teeth" : 256,
-"Theodore" : 1024
+"Theodore" : 512
 }
 passFilters = {'COLOR'}#{'COLOR'}
-bakingTypes = ["Diffuse", "Roughness"]#, "Normal"]
+bakingTypes = ["Diffuse"]#, "Roughness", "Normal"]
 
 ###### CODE ######
 
@@ -71,62 +71,74 @@ bpy.context.scene.render.engine = 'CYCLES'
 
 # We keep the states of previous objects
 for selectedItem in originalSelectedMeshes:
-    if selectedItem.type == "MESH":
-        print("Baking " + selectedItem.name)
-        #We make sure the current mesh is the only selected item
-        bpy.ops.object.select_all(action='DESELECT') 
-        selectedItem.select_set(True)
-        selectedItem.hide_render = False
-        
-        if not selectedItem.name in meshesDim:
-            bakingImg.scale(defaultBakedImageDim,defaultBakedImageDim)
-        else:
-            bakingImg.scale(meshesDim[selectedItem.name],meshesDim[selectedItem.name])
-        
-        for mat in selectedItem.data.materials:
-            #We first look for an "Image Texture" node in which we can put the baked result
-            if not "Baking" in mat.node_tree.nodes.keys():
-                mat.node_tree.nodes.new("ShaderNodeTexImage").name = "Baking"
+    try:
+        if selectedItem.type == "MESH":
+            print("Baking " + selectedItem.name)
+            #We make sure the current mesh is the only selected item
+            bpy.ops.object.select_all(action='DESELECT') 
+            selectedItem.select_set(True)
+            selectedItem.hide_render = False
+            
+            if not selectedItem.name in meshesDim:
+                bakingImg.scale(defaultBakedImageDim,defaultBakedImageDim)
+            else:
+                bakingImg.scale(meshesDim[selectedItem.name],meshesDim[selectedItem.name])
+            
+            for mat in selectedItem.data.materials:
+                if mat:
+                    #We first look for an "Image Texture" node in which we can put the baked result
+                    if not "Baking" in mat.node_tree.nodes.keys():
+                        mat.node_tree.nodes.new("ShaderNodeTexImage").name = "Baking"
+                        
+                    bakingNode = mat.node_tree.nodes["Baking"]
+                    mat.node_tree.nodes.active = bakingNode
+                    bakingNode.image = bakingImg
+            
+            #We create the baked material using the textures
+            bakedMatName = selectedItem.name + "_baked"
+
+            if bakedMatName in bpy.data.materials.keys():
+                bpy.data.materials.remove(bpy.data.materials[bakedMatName])
                 
-            bakingNode = mat.node_tree.nodes["Baking"]
-            mat.node_tree.nodes.active = bakingNode
-            bakingNode.image = bakingImg
-        
-        #We create the baked material using the textures
-        bakedMatName = selectedItem.name + "_baked"
-
-        if bakedMatName in bpy.data.materials.keys():
-            bpy.data.materials.remove(bpy.data.materials[bakedMatName])
+            bakedMat = bpy.data.materials.new(bakedMatName)
+            bakedMat.use_nodes = True
+            principledNode = bakedMat.node_tree.nodes["Principled BSDF"]
             
-        bakedMat = bpy.data.materials.new(bakedMatName)
-        bakedMat.use_nodes = True
-        principledNode = bakedMat.node_tree.nodes["Principled BSDF"]
-        
-        for currentType in bakingTypes:
-            print(" Baking type : " + currentType)
-            #We bake the image
-            bpy.ops.object.bake(type=currentType.upper(), margin=marginParam, pass_filter=set(passFilters),use_selected_to_active=False)
-            
-            #Save it
-            texPath = os.path.join(bakedMapsFolder, selectedItem.name + currentType + ".png")
-            bakingImg.save_render(texPath)
-            print(" Baked image saved")
-            
-            #And finally add it to the shader
-            #Create the node
-            texNode = bakedMat.node_tree.nodes.new("ShaderNodeTexImage")
-            #Set the params (name, image, location...)
-            texNode.name = currentType
-            texNode.label = currentType + "Tex"
-            currentInputId = principledNode.inputs.find(mapTypeToInput[currentType])
-            texNode.location.x = principledNode.location.x - 100 - principledNode.width
-            texNode.location.y = principledNode.location.y - (principledNode.height+texNode.height) + ((len(principledNode.inputs)*0.5)-currentInputId)*texNode.height*0.5
-            texNode.image = bpy.data.images.load(texPath, check_existing=True)
-            #Link it
-            bakedMat.node_tree.links.new(principledNode.inputs[currentInputId], texNode.outputs["Color"])
-            
-        selectedItem.hide_render = True
-
+            for currentType in bakingTypes:
+                print(" Baking type : " + currentType)
+                #We bake the image
+                bpy.ops.object.bake(type=currentType.upper(), margin=marginParam, pass_filter=set(passFilters),use_selected_to_active=False)
+                
+                #Save it
+                texPath = os.path.join(bakedMapsFolder, selectedItem.name + currentType + ".png")
+                bakingImg.save_render(texPath)
+                print(" Baked image saved")
+                
+                #And finally add it to the shader
+                #Create the node
+                texNode = bakedMat.node_tree.nodes.new("ShaderNodeTexImage")
+                #Set the params (name, image, location...)
+                texNode.name = currentType
+                texNode.label = currentType + "Tex"
+                currentInputId = principledNode.inputs.find(mapTypeToInput[currentType])
+                texNode.location.x = principledNode.location.x - principledNode.width * 2.0
+                texNode.location.y = principledNode.location.y - (principledNode.height+texNode.height) + ((len(principledNode.inputs)*0.5)-currentInputId)*texNode.height*0.5
+                texNode.image = bpy.data.images.load(texPath, check_existing=True)
+                #Link it
+                if currentType == 'Normal':
+                    normalMapNode = bakedMat.node_tree.nodes.new("ShaderNodeNormalMap")
+                    normalMapNode.location = texNode.location
+                    normalMapNode.space = 'OBJECT'
+                    texNode.location.x -= normalMapNode.width * 2.0
+                    bakedMat.node_tree.links.new(normalMapNode.inputs["Color"], texNode.outputs["Color"])
+                    bakedMat.node_tree.links.new(principledNode.inputs["Normal"], normalMapNode.outputs["Normal"])
+                else:
+                    bakedMat.node_tree.links.new(principledNode.inputs[currentInputId], texNode.outputs["Color"])
+                
+            selectedItem.hide_render = True
+    except Exception as e:
+        print(e)
+        break
     
 #We then restore the items that were selected
 for selectedItem in originalSelectedMeshes:
--- exportAll.py
+++ exportAll.py
@@ -5,6 +5,8 @@ import os
 
 outputFolder = 'meshes'#Relative path from .blend file
 extraCollections = []#[bpy.data.collections['Stickers']]
+useSelection = True
+applyModifiers = True
 
 ###### CODE ######
 
@@ -30,51 +32,58 @@ if not os.path.exists(absOutputFolder):
 selectedObj = bpy.context.selected_objects.copy()
 
 for obj in selectedObj:
-    if objValidToExport(obj, extraCollections) and (not obj.data in outpoutedMeshes):
-        outpoutedMeshes.append(obj.data)
-        bpy.ops.object.select_all(action='DESELECT')
-        obj.select_set(True)
-        for child in obj.children:
-            child.select_set(True)
-        if obj.parent != None:
-            if obj.parent.type == 'ARMATURE':
-                obj.parent.select_set(True)
-        
-        prevLoc = obj.location.copy()
-        obj.location *= 0
-        
-        format = 'gltf'
-        if not os.path.exists(os.path.join(absOutputFolder,format)):
-            os.makedirs(os.path.join(absOutputFolder,format))
-        
-        bpy.ops.export_scene.gltf(
-            filepath=os.path.join(absOutputFolder, format, toAlphaNum(obj.data.name) + '.' + format),
-            export_format='GLTF_EMBEDDED',
-            export_copyright="TheFamousRat",
-            export_selected=True,
-            export_apply=True,
-            export_materials=True
-        )
-        
-        format = 'obj'
-        if not os.path.exists(os.path.join(absOutputFolder,format)):
-            os.makedirs(os.path.join(absOutputFolder,format))
-        
-        bpy.ops.export_scene.obj(
-            filepath=os.path.join(absOutputFolder, format, toAlphaNum(obj.data.name) + '.' + format),
-            use_selection=True
-        )
-        
-        format = 'stl'
-        if not os.path.exists(os.path.join(absOutputFolder,format)):
-            os.makedirs(os.path.join(absOutputFolder,format))
-        
-        bpy.ops.export_mesh.stl(
-            filepath=os.path.join(absOutputFolder, format, toAlphaNum(obj.data.name) + '.' + format),
-            use_selection=True
-        )
-        
-        obj.location = prevLoc
+    try:
+        if objValidToExport(obj, extraCollections) and (not obj.data in outpoutedMeshes):
+            outpoutedMeshes.append(obj.data)
+            bpy.ops.object.select_all(action='DESELECT')
+            obj.select_set(True)
+            for child in obj.children:
+                child.select_set(True)
+            if obj.parent != None:
+                if obj.parent.type == 'ARMATURE':
+                    obj.parent.select_set(True)
+            
+            prevLoc = obj.location.copy()
+            obj.location *= 0
+            
+            format = 'gltf'
+            if not os.path.exists(os.path.join(absOutputFolder,format)):
+                os.makedirs(os.path.join(absOutputFolder,format))
+            
+            bpy.ops.export_scene.gltf(
+                filepath=os.path.join(absOutputFolder, format, toAlphaNum(obj.data.name) + '.' + format),
+                export_format='GLTF_EMBEDDED',
+                export_copyright="TheFamousRat",
+                export_selected=useSelection,
+                export_apply=applyModifiers,
+                export_colors=False,
+                export_materials=True
+            )
+            
+            format = 'obj'
+            if not os.path.exists(os.path.join(absOutputFolder,format)):
+                os.makedirs(os.path.join(absOutputFolder,format))
+            
+            bpy.ops.export_scene.obj(
+                filepath=os.path.join(absOutputFolder, format, toAlphaNum(obj.data.name) + '.' + format),
+                use_selection=useSelection,
+                use_mesh_modifiers=applyModifiers
+            )
+            
+            format = 'stl'
+            if not os.path.exists(os.path.join(absOutputFolder,format)):
+                os.makedirs(os.path.join(absOutputFolder,format))
+            
+            bpy.ops.export_mesh.stl(
+                filepath=os.path.join(absOutputFolder, format, toAlphaNum(obj.data.name) + '.' + format),
+                use_selection=useSelection,
+                use_mesh_modifiers=applyModifiers
+            )
+            
+            obj.location = prevLoc
+    except Exception as e:
+        print(e)
+        break
         
 bpy.ops.object.select_all(action='DESELECT')
 for obj in selectedObj:
